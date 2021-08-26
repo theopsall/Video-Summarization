c84929c47bc12dd899acdcfff914dec167b1371f
Theodoros Psallidas <theopsall@gmail.com>
2021-08-26 10:32:46 +0300

    initial feature extraction

---
+++ video_summarization/libs/feature_extraction.py
@@ -0,0 +1,12 @@
+#TODO move the miltimodal_movie_analysis and pyAudioAnalysis extraction on handcrafted features
+#TODO add deep feature extraction
+
+
+def extract_handcrafted_features(videos_dir: str):
+    features = []
+    return features
+
+
+def extract_deep_features(videos_dir: str):
+    features = []
+    return features
